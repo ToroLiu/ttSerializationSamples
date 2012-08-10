fc8d66a3996e61d8addb20600ed15b2fce0eaff8
Aecho <aecho1028@gmail.com>
2012-08-10 11:58:24 +0800

    First Commit.
    - xml serialization test.
    - protobuf serialization test.
    - array container serialization test.

---
+++ ttProtobufSamples/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using ProtoBuf;
+
+namespace ttProtobufSamples.Models
+{
+    [Serializable]
+    [ProtoContract]
+    public class Car : BaseCar
+    {
+        [ProtoMember(1)]
+        public Person Owner { get; set; }
+        [ProtoMember(2)]
+        public Factory Factory { get; set; }
+
+        [ProtoMember(3)]
+        public Person[] Friends { get; set; }
+
+        public Car() : base() { 
+        }
+    }
+}
---
+++ ttProtobufSamples/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using ProtoBuf;
+using System.Xml.Serialization;
+
+namespace ttProtobufSamples
+{
+    using Models;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car aCar = new Car()
+            {
+                CreatedTime = DateTime.Now,
+                Guid = System.Guid.NewGuid().ToString(),
+                Owner = new Person
+                {
+                    Id = 2,
+                    Name = "HelloKitty",
+                },
+                Factory = new Factory
+                {
+                    Guid = System.Guid.NewGuid().ToString(),
+                    Name = "Garena",
+                },
+            };
+
+            List<Person> list = new List<Person>();
+            for (int i = 0; i < 1000; i++) {
+                list.Add(new Person() { 
+                    Id = i,
+                    Name = "Soraka" + i.ToString(),
+                });
+            }
+            aCar.Friends = list.ToArray();
+
+            using (FileStream stream = File.Create("car.bin"))
+            {
+                Serializer.Serialize<Car>(stream, aCar);
+                stream.Position = 0;
+
+                Car bCar = Serializer.Deserialize<Car>(stream);
+                Console.Write(bCar.Owner.Name);
+            }
+
+            using (FileStream stream = File.Create("car.xml")) {
+                XmlSerializer xml = new XmlSerializer(typeof(Car));
+                xml.Serialize(stream, aCar);
+
+                stream.Position = 0;
+                Car bCar = (Car)xml.Deserialize(stream);
+                Console.Write(bCar.Owner.Name);
+            }
+
+            
+
+        }
+    }
+}
